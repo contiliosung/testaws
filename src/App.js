2e2bb3cfbb2333f1602b8bbf04c4d0db21a1a0db
chenhsi <chenhsi.sung@contilio.com>
2019-10-09 11:16:53 +0100

    third

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit test 5566<code>src/App.js</code> and save to reload.
+          Edit test 5566<code>src/App.js</code> and save to reload 66665555.
         </p>
         <a
           className="App-link"
